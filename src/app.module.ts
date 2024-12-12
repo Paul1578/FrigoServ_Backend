6bc85b82c4418c504c5e3edb9a3f98f0fe723f13
Padme2003 <jpp.moposita@yavirac.edu.ec>
2024-12-12 10:04:05 -0500

    prueba bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+import { UsersModule } from './modules/users/users.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DatabaseModule, ConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
